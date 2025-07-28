287299e959263ee0c7868d9e0fcd053c8f34181b
G-Avatar <oscartapdasan@gmail.com>
2025-07-28 08:08:51 +0800

    Remove if statement for featured image

--- resources/js/components/forms/FeaturedImageList.vue
+++ resources/js/components/forms/FeaturedImageList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="p-5">
-            <div v-if="props.data.featured_images.length > 0" class="flex flex-wrap border justify-center">
+            <div class="flex flex-wrap border justify-center">
                 <div class="p-2" v-for="(image, index) in props.data.featured_images">
                     <img class="h-28 w-28 border border-gray-500 rounded" :src="UrlUtil.getBaseAppUrl(`storage/images/thumbnail/${image.thumbnail}`)" :key="index" alt="Image Thumbnail"></img>
                 </div>
